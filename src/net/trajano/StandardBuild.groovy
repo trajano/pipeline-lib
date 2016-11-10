a560c7ce9fc1346e4d24ef658f7979e89948e2cd
Archimedes Trajano <arch@trajano.net>
2016-11-10 17:48:28 -0500

    added Maven build

--- src/net/trajano/StandardBuild.groovy
+++ src/net/trajano/StandardBuild.groovy
@@ -24,6 +24,59 @@ class StandardBuild implements Serializable {
             steps.checkout scm
         }
     }
+
+    /**
+     * Implements a Maven build.
+     */
+    def maven() {
+        step.node {
+            // Environment setup
+            def mvnHome = tool name:'maven-3.3.9', type: 'maven'
+            env.PATH = "${mvnHome}/bin:${env.PATH}"
+
+            checkoutScm()
+            if (env.BRANCH_NAME == "master") {
+                steps.stage(stages['build']) {
+                    mvn 'deploy site'
+                }
+                steps.stage(stages['codeQualityAnalysis']) {
+                    withSonarQubeEnv {
+                        mvn 'org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar'
+                    }
+                }
+                boolean release = false
+                steps.stage(stages['confirmRelease']) {
+                    try {
+                        timeout(time: 1, unit: 'MINUTES') {
+                            input 'Release to Central?'
+                            release = true
+                        }
+                    } catch (ignored) {
+                    }
+                }
+                if (release) {
+                    steps.stage(stages['release']) {
+                        echo "mvn 'release:prepare'"
+                        echo "mvn 'release:perform'"
+                    }
+                }
+            } else {
+                steps.stage(stages['build']) {
+                    mvn 'install site'
+                }
+            }
+
+        }
+    }
+    /**
+     * Support step to execute mvn from the shell and capture any test results.
+     */
+    @NonCPS
+    private def mvn(String args) {
+        steps.sh "mvn --batch-mode ${args}"
+        steps.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+    }
+
     /**
      * This does nothing, it is primarily used to test if the build
      * environment is working.
@@ -35,4 +88,5 @@ class StandardBuild implements Serializable {
             }
         }
     }
+
 }
